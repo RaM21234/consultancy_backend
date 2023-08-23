2294d67e374c2d456805d910b6d3d8c24a412941
ramkumar21234 <126386524+RaM21234@users.noreply.github.com>
2023-08-23 19:32:52 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const URI=`mongodb+srv://ramkumaravs01:SijuGhTUQTGfwZ1L@cluster0.qugzcs4.mongodb.net/?retryWrites=true&w=majority`
+
+
+async function connectToMongoose() {
+    try {
+      await mongoose.connect(URI);
+      console.log(`Connected to MongoDB`);
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+
+module.exports =connectToMongoose;
+
+//ramkumaravs01
+//SijuGhTUQTGfwZ1L
+//mongodb+srv://ramkumaravs01:<password>@cluster0.qugzcs4.mongodb.net/?retryWrites=true&w=majority
